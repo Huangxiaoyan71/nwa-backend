4172507e6ec3dface707a39c216bb7a72b620b56
黄晓燕 <huangxiaoyan716@gmail.com>
2021-06-09 16:55:35 +0800

    init: 初始化后端

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '25dee37de1807ea359e1bf3a47d22151'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'c252e339-5dd6-450f-a500-66972fc116dd'
+};
